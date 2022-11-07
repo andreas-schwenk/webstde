adf9f434075727be04e4ed1e0fc2ce9d7c91d341
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-07 13:29:21 +0100

    Update machine.ts

--- src/machine.ts
+++ src/machine.ts
@@ -2,12 +2,23 @@
 // LICENSE: GPLv3
 
 import { drawState } from "./draw";
-import { JSON_Signal, JSON_State, JSON_Transition } from "./interfaces";
+import {
+  JSON_Signal,
+  JSON_State,
+  JSON_StateMachine,
+  JSON_Transition,
+  Pos2D,
+} from "./interfaces";
 
 export class StateMachine {
   private id = "";
+  private signals: Signal[] = [];
   private states: State[] = [];
   private transitions: Transition[] = [];
+
+  public addSignal(signal: Signal) {
+    this.signals.push(signal);
+  }
 
   public addState(state: State) {
     this.states.push(state);
@@ -18,8 +29,44 @@ export class StateMachine {
     for (const state of this.states) state.draw(ctx);
   }
 
-  public select(x: number, y: number): void {
-    for (const state of this.states) state.select(x, y);
+  public select(pos: Pos2D): void {
+    for (const state of this.states) state.select(pos);
+  }
+
+  public getSignals(): Signal[] {
+    return this.signals;
+  }
+
+  public getStates(): State[] {
+    return this.states;
+  }
+
+  public getSelectedStates(): State[] {
+    const list: State[] = [];
+    for (const state of this.states) {
+      if (state.isSelected()) list.push(state);
+    }
+    return list;
+  }
+
+  public deleteSelection(): void {
+    const newStates: State[] = [];
+    for (const state of this.states)
+      if (state.isSelected() == false) newStates.push(state);
+    this.states = newStates;
+    this.recalculateStateIndices();
+  }
+
+  public snap(mousePos: Pos2D): Pos2D {
+    const delta = 25; // TODO: configure / parameter
+    let pos = mousePos;
+    for (const state of this.states) {
+      if (Math.abs(state.getPos().x - mousePos.x) <= delta)
+        pos.x = state.getPos().x;
+      if (Math.abs(state.getPos().y - mousePos.y) <= delta)
+        pos.y = state.getPos().y;
+    }
+    return pos;
   }
 
   private recalculateStateIndices(): void {
@@ -27,6 +74,18 @@ export class StateMachine {
     for (let i = 0; i < n; i++) {
       this.states[i].setIdx(i);
     }
+  }
+
+  public toJSON(): JSON_StateMachine {
+    return {
+      id: this.id,
+      signals: this.signals.map((e) => {
+        return e.toJSON();
+      }),
+      states: this.states.map((e) => {
+        return e.toJSON();
+      }),
+    };
   }
 }
 
@@ -36,6 +95,27 @@ export class Signal {
   private bits = 1;
   private direction = SignalDirection.Input;
   private desc = "";
+
+  public constructor(
+    id: string,
+    type: SignalType,
+    bits: number,
+    direction: SignalDirection,
+    desc = ""
+  ) {
+    this.id = id;
+    this.type = type;
+    this.bits = bits;
+    this.direction = direction;
+    this.desc = desc;
+  }
+
+  public setId(id: string): void {
+    this.id = id;
+  }
+  public getId(): string {
+    return this.id;
+  }
 
   public toJSON(): JSON_Signal {
     return {
@@ -63,54 +143,75 @@ export enum SignalDirection {
 
 export class State {
   private selected = false;
-  private x = 0;
-  private y = 0;
+  private pos: Pos2D = {
+    x: 0,
+    y: 0,
+  };
   private width = 220;
   private height = 110;
   private id = "";
   private idx = 0;
+  private code = "";
   private desc = "";
-  private mooreOutput: { [signalId: string]: string };
+  private mooreOutput: { [signalId: string]: string } = {};
 
-  public constructor(x: number, y: number, id: string, desc = "") {
-    this.x = x;
-    this.y = y;
+  public constructor(pos: Pos2D, id: string, code = "", desc = "") {
+    this.pos.x = pos.x;
+    this.pos.y = pos.y;
     this.id = id;
+    this.code = code;
     this.desc = desc;
   }
 
+  public setId(id: string) {
+    this.id = id;
+  }
+  public getId(): string {
+    return this.id;
+  }
   public getIdx(): number {
     return this.idx;
   }
   public setIdx(idx: number): void {
     this.idx = idx;
   }
-
-  public draw(ctx: CanvasRenderingContext2D) {
-    drawState(
-      ctx,
-      this.selected,
-      this.x,
-      this.y,
-      this.width,
-      this.height,
-      this.id
-    );
+  public setCode(code: string): void {
+    this.code = code;
+  }
+  public getCode(): string {
+    return this.code;
+  }
+  public setDesc(desc: string): void {
+    this.desc = desc;
+  }
+  public getDesc(): string {
+    return this.desc;
+  }
+  public getPos(): Pos2D {
+    return this.pos;
+  }
+  public isSelected(): boolean {
+    return this.selected;
   }
 
-  public select(x: number, y: number): void {
+  public draw(ctx: CanvasRenderingContext2D) {
+    drawState(ctx, this.selected, this.pos, this.width, this.height, this.id);
+  }
+
+  public select(pos: Pos2D): void {
     this.selected =
-      x >= this.x - this.width / 2 &&
-      x <= this.x + this.width / 2 &&
-      y >= this.y - this.height / 2 &&
-      y <= this.y + this.height / 2;
+      pos.x >= this.pos.x - this.width / 2 &&
+      pos.x <= this.pos.x + this.width / 2 &&
+      pos.y >= this.pos.y - this.height / 2 &&
+      pos.y <= this.pos.y + this.height / 2;
   }
 
   public toJSON(): JSON_State {
     return {
       id: this.id,
-      x: this.x,
-      y: this.y,
+      x: this.pos.x,
+      y: this.pos.y,
+      code: this.code,
       desc: this.desc,
       q: this.mooreOutput,
     };
