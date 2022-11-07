427d1a1675ac3de4026db9dcf75d0b62f00d143a
Andreas Schwenk <andreas-schwenk@users.noreply.github.com>
2022-11-07 13:29:12 +0100

    Update draw.ts

--- src/draw.ts
+++ src/draw.ts
@@ -1,17 +1,22 @@
 // webSTDE - 2022 by Andreas Schwenk <contact@compiler-construction.com>
 // LICENSE: GPLv3
 
-import { GuiState, guiState, mouseX, mouseY, machine } from ".";
+import { GuiState, guiState, machine, mousePos } from ".";
+import { Pos2D } from "./interfaces";
 
 export function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.font = "28px Arial";
+  ctx.textAlign = "left";
+  ctx.fillText("output vector = [y1, y2]", 10, 35); // TODO
 
   machine.draw(ctx);
 
   switch (guiState) {
     case GuiState.InsertMachineState:
       ctx.strokeStyle = "gray";
-      drawState(ctx, true, mouseX, mouseY, 220, 100);
+      drawState(ctx, true, machine.snap(mousePos), 220, 100);
       break;
   }
 }
@@ -19,29 +24,30 @@ export function draw(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
 export function drawState(
   ctx: CanvasRenderingContext2D,
   selected: boolean,
-  x: number,
-  y: number,
+  pos: Pos2D,
   width: number,
   height: number,
   label = ""
 ) {
   if (selected) {
     ctx.strokeStyle = "blue";
+    ctx.fillStyle = "blue";
     ctx.lineWidth = 4;
   } else {
     ctx.strokeStyle = "black";
+    ctx.fillStyle = "black";
     ctx.lineWidth = 2;
   }
 
   ctx.beginPath();
-  ctx.ellipse(x, y, width / 2, height / 2, 0, 0, 2 * Math.PI);
+  ctx.ellipse(pos.x, pos.y, width / 2, height / 2, 0, 0, 2 * Math.PI);
   ctx.stroke();
 
-  ctx.moveTo(x - width / 2, y);
-  ctx.lineTo(x + width / 2, y);
+  ctx.moveTo(pos.x - width / 2, pos.y);
+  ctx.lineTo(pos.x + width / 2, pos.y);
   ctx.stroke();
 
   ctx.font = "24px Arial";
   ctx.textAlign = "center";
-  ctx.fillText(label, x, y - height / 6);
+  ctx.fillText(label, pos.x, pos.y - height / 6);
 }
